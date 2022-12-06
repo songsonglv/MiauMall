3743017ae40fb2ecdc284c86eef3f191355beefc
吕松松 <www.13460720766@163.com>
2022-12-06 10:09:48 +0800

    Initial Commit

---
+++ MiauMall/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  MiauMall
+//
+//  Created by 吕松松 on 2022/12/6.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MiauMallUITests/MiauMallUITestsLaunchTests.m
@@ -0,0 +1,37 @@
+//
+//  MiauMallUITestsLaunchTests.m
+//  MiauMallUITests
+//
+//  Created by 吕松松 on 2022/12/6.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MiauMallUITestsLaunchTests : XCTestCase
+
+@end
+
+@implementation MiauMallUITestsLaunchTests
+
++ (BOOL)runsForEachTargetApplicationUIConfiguration {
+    return YES;
+}
+
+- (void)setUp {
+    self.continueAfterFailure = NO;
+}
+
+- (void)testLaunch {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app launch];
+
+    // Insert steps here to perform after app launch but before taking a screenshot,
+    // such as logging into a test account or navigating somewhere in the app
+
+    XCTAttachment *attachment = [XCTAttachment attachmentWithScreenshot:XCUIScreen.mainScreen.screenshot];
+    attachment.name = @"Launch Screen";
+    attachment.lifetime = XCTAttachmentLifetimeKeepAlways;
+    [self addAttachment:attachment];
+}
+
+@end
